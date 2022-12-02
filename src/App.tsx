eb658a4425b94a2bfdbee4d9d686461b88f02e65
shareef99 <nadeemshareef934@gmail.com>
2022-12-02 10:58:47 +0530

    Setup 🔧: Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,53 @@
+import {
+  RouterProvider,
+  createBrowserRouter,
+  Navigate,
+} from "react-router-dom";
+
 function App() {
-  return <div>hello World</div>;
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <>Home</>,
+      children: [
+        {
+          path: "",
+          element: <Navigate to="/home" />,
+        },
+        {
+          path: "home",
+          element: (
+            <div className="flex h-full items-center justify-center text-3xl font-medium tracking-wide text-p-blue-dark">
+              ADMIN DASHBOARD UNDER MAINTENANCE
+            </div>
+          ),
+        },
+      ],
+    },
+    {
+      path: "/auth",
+      children: [
+        {
+          path: "signin",
+          element: <>Sign In</>,
+        },
+        {
+          path: "signup",
+          element: <>Sign Up</>,
+        },
+        {
+          path: "*",
+          element: <Navigate to="/auth/signin" />,
+        },
+        {
+          path: "",
+          element: <Navigate to="/auth/signin" />,
+        },
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
